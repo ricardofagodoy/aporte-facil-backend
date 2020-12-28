0114bfeb9c48f74aa3b2dcbb7fce2a5a8f1cbfb0
ricardofagodoy <ricardofagodoy@gmail.com>
2020-12-28 20:23:00 -0300

    Adding app engine configs

--- src/main/java/com/aportefacil/backend/controllers/interceptors/SecurityInterceptor.java
+++ src/main/java/com/aportefacil/backend/controllers/interceptors/SecurityInterceptor.java
@@ -8,11 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class SecurityInterceptor implements HandlerInterceptor {
 
     private final String loggedField;
+    private final Set<String> METHODS = new HashSet<>(Arrays.asList("POST", "GET", "PATCH", "DELETE"));
 
     public SecurityInterceptor(@Value("${logged.field}") String loggedField) {
         this.loggedField = loggedField;
@@ -26,7 +30,7 @@ public class SecurityInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession(false);
 
         // Not logged In
-        if (session == null || session.getAttribute(loggedField) == null) {
+        if (METHODS.contains(request.getMethod()) && (session == null || session.getAttribute(loggedField) == null)) {
             response.getWriter().write("Not authorized. Call /login");
             response.setStatus(403);
 
